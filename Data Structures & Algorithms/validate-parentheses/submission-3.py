85f59730a4f5d5593ecca7fd609fdf17251f8115
Ricardo Vinicius <108153768+ricardovinicius@users.noreply.github.com>
2026-04-24 17:45:12 -0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        close_to_open_map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in close_to_open_map:
+                if not stack or stack.pop() != close_to_open_map[char]:
+                    return False
+            else:
+                stack.append(char)
+
+
+        return False if len(stack) else True
+            
+
